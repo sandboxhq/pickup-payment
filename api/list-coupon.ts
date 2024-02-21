97ff15387d2ab0be81cfe5fdfff5251eef890d98
Jom Winston Magbag <bjomwinston0107@gmail.com>
2024-02-21 11:12:57 +0800

    coupon implement

--- api/list-coupon.ts
+++ api/list-coupon.ts
@@ -7,11 +7,19 @@ const stripe = new Stripe(`${process.env.STRIPE_API_KEY}`);
 
 export default async function listCoupons(req: VercelRequest, res: VercelResponse) {
     try {
-        const coupons = await stripe.promotionCodes.list();
+        const coupons = await stripe.coupons.list();
 
         const couponList = coupons.data.map((code) => ({
             code
         }));
+
+        const couponId = 'cfOKDuin';
+        await stripe.coupons.update(couponId, {
+            metadata: {
+                times_redeemed: 0
+            },
+        });
+
 
         res.json({ coupons: couponList });
     } catch (error) {
--- api/payment-sheet.ts
+++ api/payment-sheet.ts
@@ -18,65 +18,82 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
 
 async function handlePayment(req: VercelRequest, res: VercelResponse) {
   try {
-    const { topUp } = req.body;
+    const { topUp, code } = req.body;
+    const customer = await stripe.customers.create();
+    let paymentAmount = topUp;
+    if (code) {
+      const coupon = await stripe.coupons.retrieve(code);
+      if (!coupon || !coupon.valid) {
+        return res.status(400).json({ error: 'Invalid coupon' });
+      }
 
-    // const customer = await stripe.customers.create();
+      await stripe.coupons.update(code, {
+        metadata: {
+          times_redeemed: + 1,
+        }
+      });
 
+      if (coupon.percent_off === 100) {
+        paymentAmount = 3000;
+      } else if (coupon.percent_off) {
+        const calcuAmount = (coupon.percent_off / 100) * topUp;
+        if (calcuAmount <= 3000) {
+          paymentAmount = 3000
+        } else {
+          paymentAmount = calcuAmount;
+        }
+      }
 
-    // if (code) {
-
-    //   const coupons = await stripe.promotionCodes.list({ code: code });
-
-
-    //   if (coupons.data.length === 0 || coupons.data[0].active === false || coupons.data[0].coupon.valid === false) {
-    //     res.status(400).json({ error: 'Invalid or inactive coupon code' });
-    //     return;
-    //   }
-    //   const coupon = await stripe.promotionCodes.retrieve(coupons.data[0].id);
-
-    //   await stripe.promotionCodes.update(coupon.id, {
-    //     metadata: {
-    //       times_redeemed: coupon.metadata.times_redeemed + 1,
-    //     },
-    //   });
-    //   console.log('done');
-    // }
-
-    // const ephemeralKey = await stripe.ephemeralKeys.create(
-    //   { customer: customer.id },
-    //   { apiVersion: '2023-10-16' }
-    // );
-
-    // const paymentIntent = await stripe.paymentIntents.create({
-    //   amount: topUp,
-    //   currency: 'php',
-    //   customer: customer.id,
-    //   automatic_payment_methods: {
-    //     enabled: true,
-    //   },
-    // });
-    const payment = await stripe.checkout.sessions.create({
-      line_items: [{
-        price_data: {
-          currency: 'php',
-          product_data: {
-            name: 'ticket',
+      const payment = await stripe.checkout.sessions.create({
+        line_items: [{
+          price_data: {
+            currency: 'php',
+            product_data: {
+              name: 'ticket',
+            },
+            unit_amount: paymentAmount,
           },
-          unit_amount: topUp,
-        },
-        quantity: 1
-      }],
-      mode: 'payment',
-      ui_mode: 'embedded',
-      return_url: 'http://localhost:8000/'
-    })
-    res.json({
-      // paymentIntent: paymentIntent.client_secret,
-      // ephemeralKey: ephemeralKey.secret,
-      // customer: customer.id,
-      clientsecret: payment.client_secret
-      // publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    });
+          quantity: 1
+        }],
+        mode: 'payment',
+        ui_mode: 'embedded',
+        return_url: 'http://localhost:8080/',
+      })
+      res.json({
+        // paymentIntent: paymentIntent.client_secret,
+        // ephemeralKey: ephemeralKey.secret,
+        paymentIntent: payment.client_secret,
+        customer: customer.id,
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+        message: `The user pay amount:${paymentAmount / 100}pesos`
+      });
+    } else {
+      const payment = await stripe.checkout.sessions.create({
+        line_items: [{
+          price_data: {
+            currency: 'php',
+            product_data: {
+              name: 'ticket',
+            },
+            unit_amount: topUp,
+          },
+          quantity: 1
+        }],
+        mode: 'payment',
+        ui_mode: 'embedded',
+        return_url: 'http://localhost:8000/'
+      })
+      res.json({
+        // paymentIntent: paymentIntent.client_secret,
+        // ephemeralKey: ephemeralKey.secret,
+        paymentIntent: payment.client_secret,
+        customer: customer.id,
+        clientsecret: payment.client_secret,
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+        message: `The user pay amount: ${topUp / 100} pesos`
+      });
+    }
+
   } catch (error) {
     console.error('Error processing payment:', error);
     res.status(500).json({ error: 'Internal Server Error' });
